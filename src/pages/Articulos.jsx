331c5cb4a97b9c0aa1d200f253db3d362a8e0ef2
CoboMaster <cobomaster1@gmail.com>
2025-06-16 13:54:30 +0200

    Recuperacion fallo netlify 2

--- src/pages/Articulos.jsx
+++ src/pages/Articulos.jsx
@@ -1,6 +1,6 @@
 import { useParams, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import './inicio.css';
+import './Inicio.css';
 
 function Articulo() {
   const { id } = useParams();
